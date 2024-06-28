6d32a67800abd270060c2c6cad3bc43d24710130
Barış karakuş <bariscan9788@gmail.com>
2024-06-28 03:58:49 +0300

    .

---
+++ dynamic programming/198. House Robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        @cache
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            return max(nums[i] + dfs(i + 2), dfs(i + 1))
+
+        return dfs(0)
