9719931fe7872fded0b64bae5607ff3fcb2dcda0
Barış karakuş <bariscan9788@gmail.com>
2024-10-28 04:48:58 +0300

    initial commit

---
+++ dynamic programming/2501. Longest Square Streak in an Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        
+        dp = {}
+        for i in nums:
+            dp[i] = 0
+        
+        for i in nums:
+            val = int(i ** 2)    
+            if val in dp:
+                dp[val] = max(dp[val], dp[i] + 1)
+        
+        res = max(dp.values())
+        return -1 if not res else res + 1
